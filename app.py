9798a489d65136710e39d26b61e6814f9aebae55
Rizko Imsar <110739010+rizko77@users.noreply.github.com>
2025-02-09 16:11:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,8 @@ if __name__ == "__main__":
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Origin', '*')  # CORS untuk semua origin
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')  # Metode yang diizinkan
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')  # Header yang diizinkan
     return response
+
